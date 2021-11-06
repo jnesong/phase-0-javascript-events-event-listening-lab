a1f54f6f117a216383fff9eedc9cef6e7e9635f2
Jenny Chau <jnechau@gmail.com>
2021-11-06 11:26:00 -0400

    completed assignment pt 2

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-function addingEventListener() {
+// function addingEventListener() {
 
-const input = document.getElementById('input');
+// const input = document.getElementById('input');
 
-function clickAlert() {
-  alert('I was clicked!');
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert); 
+// }
+
+
+// new start on 11.6.21
+
+function addingEventListener(){
+  const variable = document.querySelector('#input');
+  const clickAlert = () => {alert ('I was clicked!')};
+  variable.addEventListener('click', clickAlert);
 }
 
-input.addEventListener('click', clickAlert); 
-}
+addingEventListener()
